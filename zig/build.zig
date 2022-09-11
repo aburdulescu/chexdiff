1f6f4fae0a8afb5c0ce0a5914acad7ca8815dac9
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-09-11 23:15:54 +0300

    Always single-threaded

--- zig/build.zig
+++ zig/build.zig
@@ -6,14 +6,13 @@ pub fn build(b: *std.build.Builder) void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const single_threaded = b.option(bool, "single-threaded", "Build artifacts that run in single threaded mode") orelse true;
-    const strip = b.option(bool, "strip", "Omit debug information") orelse true;
+    const strip = b.option(bool, "strip", "Omit debug information") orelse false;
 
     const exe = b.addExecutable("chexdiff", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.strip = strip;
-    exe.single_threaded = single_threaded;
+    exe.single_threaded = true;
     exe.install();
 
     const run_cmd = exe.run();
