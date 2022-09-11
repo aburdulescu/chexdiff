61e2d8d855bed53514ceb2017384a7a9957cc1b6
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-09-12 00:08:28 +0300

    finish implementation and add some tests

--- zig/build.zig
+++ zig/build.zig
@@ -15,12 +15,10 @@ pub fn build(b: *std.build.Builder) void {
     exe.single_threaded = true;
     exe.install();
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    const exe_tests = b.addTest("src/main.zig");
+    exe_tests.setTarget(target);
+    exe_tests.setBuildMode(mode);
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&exe_tests.step);
 }
--- zig/src/main.zig
+++ zig/src/main.zig
@@ -62,10 +62,68 @@ pub fn main() !void {
         fatal("'{s}' has invalid length", .{second});
     }
 
-    try stdout.print("hello\n", .{});
+    try hexstr_process(first, second);
+    try hexstr_process(second, first);
 }
 
 pub fn fatal(comptime format: []const u8, args: anytype) noreturn {
     std.log.err(format, args);
     std.process.exit(1);
+}
+
+const RED = "\x1b[31;1m";
+const RESET = "\x1b[0m";
+
+fn isUpper(v: u8) bool {
+    return v >= 'A' and v <= 'F';
+}
+
+test "isUpper" {
+    const uppers = "ABCDEF";
+    for (uppers) |c| {
+        try std.testing.expect(isUpper(c));
+    }
+    const not_uppers = "abcdef0123456789xyz";
+    for (not_uppers) |c| {
+        try std.testing.expect(!isUpper(c));
+    }
+}
+
+fn isCharEq(l: u8, r: u8) bool {
+    const magic = 'a' - 'A';
+    const ll = if (isUpper(l)) l + magic else l;
+    const rr = if (isUpper(r)) r + magic else r;
+    return ll == rr;
+}
+
+test "isCharEq" {
+    try std.testing.expect(isCharEq('A', 'a'));
+    try std.testing.expect(!isCharEq('A', 'b'));
+}
+
+fn isHexDigitEq(l: []const u8, r: []const u8) bool {
+    return isCharEq(l[0], r[0]) and isCharEq(l[1], r[1]);
+}
+
+test "isHexDigitEq" {
+    try std.testing.expect(isHexDigitEq("AA", "AA"));
+    try std.testing.expect(isHexDigitEq("AA", "aa"));
+    try std.testing.expect(!isHexDigitEq("AA", "BB"));
+    try std.testing.expect(!isHexDigitEq("AA", "bb"));
+}
+
+fn hexstr_process(self: []const u8, other: []const u8) !void {
+    const cmn_len = if (self.len < other.len) self.len else other.len;
+    var i: usize = 0;
+    while (i < cmn_len) : (i += 2) {
+        if (isHexDigitEq(self[i .. i + 2], other[i .. i + 2])) {
+            try stdout.print("{s}", .{self[i .. i + 2]});
+        } else {
+            try stdout.print("{s}{s}{s}", .{ RED, self[i .. i + 2], RESET });
+        }
+    }
+    if (self.len > other.len) {
+        try stdout.print("{s}{s}{s}", .{ RED, self[other.len..], RESET });
+    }
+    try stdout.print("\n", .{});
 }
