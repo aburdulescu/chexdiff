f1dd62b37233b115ad90d5132367f70961ec3b33
Andrei Burdulescu <andrei.burdulescu@pm.me>
2022-09-12 17:18:17 +0300

    Use ReleaseSafe and bump version

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    b.setPreferredReleaseMode(.ReleaseSmall);
+    b.setPreferredReleaseMode(.ReleaseSafe);
 
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
